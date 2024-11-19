266e5622647a9c73ee27493ccb34502304c42dee
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2024-11-18 22:31:13 -0800

    test

--- lab2framework/agents/guma.py
+++ lab2framework/agents/guma.py
@@ -16,7 +16,6 @@ class Guma(agent.Agent):
         self.explanation = []
     
     def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints, hits, cards_left):
-        # Track hints for all cards in other players' hands
         for player,hand in enumerate(hands):
             for card_index,_ in enumerate(hand):
                 if (player,card_index) not in self.hints:
@@ -39,7 +38,7 @@ class Guma(agent.Agent):
                 
         if potential_discards:
             return Action(DISCARD, card_index=random.choice(potential_discards))
-         
+        
         playables = []        
         for player,hand in enumerate(hands):
             if player != nr:
@@ -77,45 +76,116 @@ class Guma(agent.Agent):
                 return Action(HINT_COLOR, player=player, color=hands[player][card_index].color)
             
             playables = playables[1:]
+        
+        # if there is anything that could be discardable, discard it
+        if potential_discards:
+            return Action(DISCARD, card_index=random.choice(potential_discards))
 
-        # Play aggressively if game is near end
-        if cards_left < 5 or hits >= 2:
-            for i, k in enumerate(my_knowledge):
-                if util.maybe_playable(k, board):
-                    return Action(PLAY, card_index=i)  
+        # if there are no other moves available, discard the middle card
+        return Action(DISCARD, card_index = 2)
+        
+        #Random discard as last resort
+        #return random.choice(util.filter_actions(DISCARD, valid_actions))
+        # # Track hints for all cards in other players' hands
+        # for player,hand in enumerate(hands):
+        #     for card_index,_ in enumerate(hand):
+        #         if (player,card_index) not in self.hints:
+        #             self.hints[(player,card_index)] = set()
+        # known = [""]*5
+        # for h in self.hints:
+        #     pnr, card_index = h 
+        #     if pnr != nr:
+        #         known[card_index] = str(list(map(format_hint, self.hints[h])))
+        # self.explanation = [["hints received:"] + known]
+
+        # my_knowledge = knowledge[nr]
+        
+        # potential_discards = []
+        # for i,k in enumerate(my_knowledge):
+        #     if util.is_playable(k, board):
+        #         return Action(PLAY, card_index=i)
+        #     if util.is_useless(k, board):    
+        #         potential_discards.append(i)
+                
+        # if potential_discards:
+        #     return Action(DISCARD, card_index=random.choice(potential_discards))
+         
+        # playables = []        
+        # for player,hand in enumerate(hands):
+        #     if player != nr:
+        #         for card_index,card in enumerate(hand):
+        #             if card.is_playable(board):                              
+        #                 playables.append((player,card_index))
+        
+        # playables.sort(key=lambda which: -hands[which[0]][which[1]].rank)
+        # while playables and hints > 0:
+        #     player,card_index = playables[0]
+        #     knows_rank = True
+        #     real_color = hands[player][card_index].color
+        #     real_rank = hands[player][card_index].rank
+        #     k = knowledge[player][card_index]
+            
+        #     hinttype = [HINT_COLOR, HINT_RANK]
+            
+            
+        #     for h in self.hints[(player,card_index)]:
+        #         hinttype.remove(h)
+            
+        #     t = None
+        #     if hinttype:
+        #         t = random.choice(hinttype)
+            
+        #     if t == HINT_RANK:
+        #         for i,card in enumerate(hands[player]):
+        #             if card.rank == hands[player][card_index].rank:
+        #                 self.hints[(player,i)].add(HINT_RANK)
+        #         return Action(HINT_RANK, player=player, rank=hands[player][card_index].rank)
+        #     if t == HINT_COLOR:
+        #         for i,card in enumerate(hands[player]):
+        #             if card.color == hands[player][card_index].color:
+        #                 self.hints[(player,i)].add(HINT_COLOR)
+        #         return Action(HINT_COLOR, player=player, color=hands[player][card_index].color)
+            
+        #     playables = playables[1:]
+
+        # # Play aggressively if game is near end
+        # if cards_left < 5 or hits >= 2:
+        #     for i, k in enumerate(my_knowledge):
+        #         if util.maybe_playable(k, board):
+        #             return Action(PLAY, card_index=i)  
                   
-        # Give hints
-        if hints > 0:
-            # Play clue
-            for player, hand in enumerate(hands):
-                if player != nr:
-                    for card_index, card in enumerate(hand):
-                        if card.is_playable(board):
-                            play_hint = util.filter_actions(HINT_COLOR, valid_actions) + util.filter_actions(HINT_RANK, valid_actions)
-                            for hint in play_hint:
-                                if hint.player == player and (hint.color == card.color or hint.rank == card.rank):
-                                    return hint
+        # # Give hints
+        # if hints > 0:
+        #     # Play clue
+        #     for player, hand in enumerate(hands):
+        #         if player != nr:
+        #             for card_index, card in enumerate(hand):
+        #                 if card.is_playable(board):
+        #                     play_hint = util.filter_actions(HINT_COLOR, valid_actions) + util.filter_actions(HINT_RANK, valid_actions)
+        #                     for hint in play_hint:
+        #                         if hint.player == player and (hint.color == card.color or hint.rank == card.rank):
+        #                             return hint
 
-            # Save clue       
-            for player, hand in enumerate(hands):
-                if player != nr:  # Consider only other players' hands
-                    for card_index, card in enumerate(hand):
-                        # Determine if the card is critical:
-                        # - Rank 5 cards are always critical
-                        # - A card is unique if all other copies are in trash or played
-                        total_count = 3 if card.rank == 1 else (2 if card.rank in [2, 3, 4] else 1)
-                        count_in_trash = sum(1 for c in trash if c == card)
-                        count_in_played = sum(1 for c in played if c == card)
-                        is_critical = (card.rank == 5) or ((count_in_trash + count_in_played) >= (total_count - 1))
+        #     # Save clue       
+        #     for player, hand in enumerate(hands):
+        #         if player != nr:  # Consider only other players' hands
+        #             for card_index, card in enumerate(hand):
+        #                 # Determine if the card is critical:
+        #                 # - Rank 5 cards are always critical
+        #                 # - A card is unique if all other copies are in trash or played
+        #                 total_count = 3 if card.rank == 1 else (2 if card.rank in [2, 3, 4] else 1)
+        #                 count_in_trash = sum(1 for c in trash if c == card)
+        #                 count_in_played = sum(1 for c in played if c == card)
+        #                 is_critical = (card.rank == 5) or ((count_in_trash + count_in_played) >= (total_count - 1))
 
-                        if is_critical:
-                            # Collect valid save hints
-                            save_hint = util.filter_actions(HINT_COLOR, valid_actions) + util.filter_actions(HINT_RANK, valid_actions)
-                            for hint in save_hint:
-                                if hint.player == player and (hint.color == card.color or hint.rank == card.rank):
-                                    return hint
+        #                 if is_critical:
+        #                     # Collect valid save hints
+        #                     save_hint = util.filter_actions(HINT_COLOR, valid_actions) + util.filter_actions(HINT_RANK, valid_actions)
+        #                     for hint in save_hint:
+        #                         if hint.player == player and (hint.color == card.color or hint.rank == card.rank):
+        #                             return hint
                             
-        return random.choice(util.filter_actions(DISCARD, valid_actions))
+        # return random.choice(util.filter_actions(DISCARD, valid_actions))
 
     def inform(self, action, player):
         if action.type in [PLAY, DISCARD]:
